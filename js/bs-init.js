989a7e4bd82ae80ee491f404bcdd3c77bc8cac0d
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-04-20 11:37:35 +0100

    Set the script in document.ready tag so null error should fade away

--- js/bs-init.js
+++ js/bs-init.js
@@ -4,35 +4,40 @@
   });
 })(jQuery);
 
-var forestBackground = document.getElementById('forestBackground');
+(function($){
+  $(document).ready(function(){
+    var forestBackground = document.getElementById('forestBackground');
 
-var myLazyLoad = new LazyLoad({
-  elements_selector: ".lazy"
-});
+    var myLazyLoad = new LazyLoad({
+      elements_selector: ".lazy"
+    });
 
-function loadImgs() {
-  if (window.matchMedia("only screen and (max-width:991px)").matches){
-    var src1 = forestBackground.getAttribute('data-src-small');
-  } else {
-    var src1 = forestBackground.getAttribute('data-src-large');
-  }
+    function loadImgs() {
+      if (window.matchMedia("only screen and (max-width:991px)").matches){
+        var src1 = forestBackground.getAttribute('data-src-small');
+      } else {
+        var src1 = forestBackground.getAttribute('data-src-large');
+      }
 
-  forestBackground.setAttribute('data-src', src1);
+      forestBackground.setAttribute('data-src', src1);
 
-  forestBackground.removeAttribute('data-was-processed');
+      forestBackground.removeAttribute('data-was-processed');
 
-  myLazyLoad.update();
-}
+      myLazyLoad.update();
+    }
 
-loadImgs();
-
-var lastWindowSize = window.innerWidth;
-
-window.onresize = function (event){
-  var currentWindowSize = window.innerWidth;
-  if ((lastWindowSize <= 991 && currentWindowSize > 991) || (lastWindowSize > 991 && currentWindowSize <= 991)){
     loadImgs();
-  }
 
-  lastWindowSize = currentWindowSize;
-};
+    var lastWindowSize = window.innerWidth;
+
+    window.onresize = function (event){
+      var currentWindowSize = window.innerWidth;
+      if ((lastWindowSize <= 991 && currentWindowSize > 991) || (lastWindowSize > 991 && currentWindowSize <= 991)){
+        loadImgs();
+      }
+
+      lastWindowSize = currentWindowSize;
+    };
+
+  });
+})(jQuery);
