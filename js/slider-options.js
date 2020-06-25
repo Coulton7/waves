c9b0c9cb41c573ea2aea5f3ce479d98debfeb742
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-06-25 14:46:40 +0100

    Adding new javascript and html for new slider

---
+++ js/slider-options.js
@@ -0,0 +1,33 @@
+(function($) {
+  $(document).ready(function() {
+
+    $('.flexslider').flexslider({
+      controlNav: false,
+      directionNav: false
+
+    });
+
+    $('.controlNav').flexslider({
+      controlNav: true,
+      directionNav: false
+    });
+
+    $('.carousel-slider').flexslider({
+      controlNav: true,
+      directionNav: true,
+      animation: slide,
+      easing: swing,
+      animationLoop: true,
+      pauseOnHover: true,
+      maxItems: 5,
+      move: 1
+
+    });
+
+    $('.directionNav').flexslider({
+      controlNav: false,
+      directionNav: true
+    });
+
+  });
+})(jQuery)
