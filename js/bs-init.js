c13cab2441b49a20183e573b90c30a9fc537d32f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-01-13 09:42:59 +0000

    Reset back to normal later development of script

--- js/bs-init.js
+++ js/bs-init.js
@@ -4,15 +4,6 @@
   });
 });
 
-(function($) {
-  $(document).ready(function() {
-		$(".dropdown-menu li a").click(function(){
-  		$(this).parents(".dropdown").find('.tab-drop').html($(this).text() + ' <span class="caret"></span>');
-  		$(this).parents(".dropdown").find('.tab-drop').val($(this).data('value'));
-		});
-  });
-});
-
   document.addEventListener("DOMContentLoaded", function() {
     var lazyloadImages;
 
