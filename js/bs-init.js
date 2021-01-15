e5bf83ecc814423c2896b398405f61b43ef42e65
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-01-15 10:53:30 +0000

    Testing a variation fix for another issue that could occur

--- js/bs-init.js
+++ js/bs-init.js
@@ -8,7 +8,7 @@ $(function(){
   var hash = window.location.hash;
   hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 
-  $('.nav-tabs a').click(function (e) {
+  $('.nav-tabs a').on('shown.bs.tab', function (e) {
     $(this).tab('show');
     var scrollmem = $('body').scrollTop() || $('html').scrollTop();
     window.location.hash = this.hash;
