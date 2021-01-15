5ac2c59df2360c8d811c70f4826e41df9078a31f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-01-15 10:36:02 +0000

    Adding a js for the tabs so it can load off a link

--- js/bs-init.js
+++ js/bs-init.js
@@ -4,6 +4,16 @@
   });
 });
 
+var url = document.location.toString();
+if (url.match('#')) {
+    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+}
+
+// Change hash for page-reload
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+})
+
   document.addEventListener("DOMContentLoaded", function() {
     var lazyloadImages;
 
