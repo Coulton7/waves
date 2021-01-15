c39081fb3c12878fa54e82114745fa0ab5c3f611
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-01-15 10:42:51 +0000

    Adinga new script to see if it will open with the correct tab on links

--- js/bs-init.js
+++ js/bs-init.js
@@ -4,16 +4,17 @@
   });
 });
 
-var url = document.location.toString();
-if (url.match('#')) {
-    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
-}
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 
-// Change hash for page-reload
-$('.nav-tabs a').on('shown.bs.tab', function (e) {
-    window.location.hash = e.target.hash;
-})
-
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+});
   document.addEventListener("DOMContentLoaded", function() {
     var lazyloadImages;
 
