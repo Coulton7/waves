61d8cb6d75ba4cab8a8b7ff3d13328cfaad949b8
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-24 11:26:29 +0000

    Changing the height so it fits with out nav css

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -1,7 +1,7 @@
 function autocollapse() {
   var navbar = $('#autocollapse');
   navbar.removeClass('collapsed'); // set standart view
-  if(navbar.innerHeight() > 50) // check if we've got 2 lines
+  if(navbar.innerHeight() > 60) // check if we've got 2 lines
     navbar.addClass('collapsed'); // force collapse mode
 }
 
