41d72c4a75e68af004589fadc1b2a0ff591ce7b1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-24 11:10:53 +0000

    Adding the new collapse function

---
+++ js/menu-collapse.js
@@ -0,0 +1,9 @@
+function autocollapse() {
+  var navbar = $('#autocollapse');
+  navbar.removeClass('collapsed'); // set standart view
+  if(navbar.innerHeight() > 50) // check if we've got 2 lines
+    navbar.addClass('collapsed'); // force collapse mode
+}
+
+$(document).on('ready', autocollapse);
+$(window).on('resize', autocollapse);
