de24c5ea2b1ce6e6ef41ddc326d8fd7185eb1745
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-29 11:29:16 +0000

    Adjusting the script to fire as function and formatted the js

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -1,21 +1,29 @@
 function autocollapse() {
   var navbar = $('#autocollapse');
   navbar.removeClass('collapsed'); // set standart view
-  if(navbar.innerHeight() > 60) // check if we've got 2 lines
+  if (navbar.innerHeight() > 60) // check if we've got 2 lines
     navbar.addClass('collapsed'); // force collapse mode
 }
 
 (function($) {
-  var overlayElem = document.querySelector('.overlay-btn');
-  var menuItem = document.querySelector('.last');
+  function menuOverlay() {
+    var overlayElem = document.querySelector('.overlay-btn');
+    var menuItem = document.querySelector('.last');
 
-  var overlayBound = overlayElem.getBoundingClientRect();
-  var menuBound = menuItem.getBoundingClientRect();
+    var overlayBound = overlayElem.getBoundingClientRect();
+    var menuBound = menuItem.getBoundingClientRect();
 
-  if (menuBound.top + menuBound.height > overlayBound.top
-      && menuBound.left + menuBound.width > overlayBound.left
-      && menuBound.bottom - menuBound.height < overlayBound.bottom
-      && menuBound.right - menuBound.width < overlayBound.right) {
-          overlayElem.addClass("display-none");
-      }
-    });(jQuery)
+    if (menuBound.top + menuBound.height > overlayBound.top &&
+      menuBound.left + menuBound.width > overlayBound.left &&
+      menuBound.bottom - menuBound.height < overlayBound.bottom &&
+      menuBound.right - menuBound.width < overlayBound.right) {
+      overlayElem.addClass("display-none");
+    }
+  }
+
+  $(document).on('ready', menuOverlay);
+  $(window).on('resize', menuOverlay);
+  $(document).on('ready', autocollapse);
+  $(window).on('resize', autocollapse);
+});
+(jQuery)
