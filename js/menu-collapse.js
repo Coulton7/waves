5deb2167072f9b7a2014f35170235428045eb48e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-11-29 11:31:47 +0000

    Adjusting the script again for test results

--- js/menu-collapse.js
+++ js/menu-collapse.js
@@ -1,11 +1,12 @@
-function autocollapse() {
-  var navbar = $('#autocollapse');
-  navbar.removeClass('collapsed'); // set standart view
-  if (navbar.innerHeight() > 60) // check if we've got 2 lines
-    navbar.addClass('collapsed'); // force collapse mode
-}
-
 (function($) {
+
+  function autocollapse() {
+    var navbar = $('#autocollapse');
+    navbar.removeClass('collapsed'); // set standart view
+    if (navbar.innerHeight() > 60) // check if we've got 2 lines
+      navbar.addClass('collapsed'); // force collapse mode
+  }
+
   function menuOverlay() {
     var overlayElem = document.querySelector('.overlay-btn');
     var menuItem = document.querySelector('.last');
