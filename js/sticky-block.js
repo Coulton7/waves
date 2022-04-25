eadd647e2cf2cd1cef388153c21fbd183d23cabc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2022-04-25 11:06:35 +0100

    Adding new js ready for the test of the menu block

---
+++ js/sticky-block.js
@@ -0,0 +1,29 @@
+(function($) {
+
+  $(document).ready(function() {
+    "use strict";
+    var block = $('.float-block'),
+      nav = $('.side-menu'),
+      windowScreen = $(window),
+      navOffset = nav.offset().bottom,
+      blockWidth = $(".region-sidebar-first").width(),
+      previousScroll = 0;
+
+    if (windowScreen.scrollTop() >= navOffset && !block.hasClass('sticky')) {
+      block.addClass('sticky');
+      block.css('width', blockWidth + 'px');
+    }
+    $(window).on('scroll', function() {
+
+      if (windowScreen.scrollTop() >= navOffset && !block.hasClass('sticky')) {
+        block.addClass('sticky');
+        block.css('width', blockWidth + 'px');
+      }
+
+      if (windowScreen.scrollTop() < navOffset && block.hasClass('sticky')) {
+        block.removeClass('sticky');
+      }
+
+    });
+  });
+})(jQuery);
