e59c43f63321cfd51e02e6615340b9ffb5c7de12
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-10-17 16:08:05 +0100

    Small adjustment to the search-result

--- js/site-search.js
+++ js/site-search.js
@@ -84,15 +84,15 @@ document.addEventListener("DOMContentLoaded", function() {
             templates:{
                 item: data => `
                 <div class="search-result">
-                    <p class="h3 ${data.title ? '' : 'd-none'}">${data.title}</p>
-                    <p class="h3 ${data.name_1 ? '' : 'd-none'}">${data.name_1}</p>
-                    <p class="lead ${data.type ? '' : 'd-none'}">${data.type}</p>
-                    <p class="lead ${data.vid ? '' : 'd-none'}">${data.vid}</p>
-                    <p class=${data.description ? '' : 'd-none'}>${instantsearch.snippet({
+                    <p class="h3 ${data.title ? '' : 'display-none'}">${data.title}</p>
+                    <p class="h3 ${data.name_1 ? '' : 'display-none'}">${data.name_1}</p>
+                    <p class="lead ${data.type ? '' : 'display-none'}">${data.type}</p>
+                    <p class="lead ${data.vid ? '' : 'display-none'}">${data.vid}</p>
+                    <p class=${data.description ? '' : 'display-none'}>${instantsearch.snippet({
                         attribute: "description",
                         hit: data
                     })}</p>
-                    <p class=${data.body ? '' : 'd-none'}>${instantsearch.snippet({
+                    <p class=${data.body ? '' : 'display-none'}>${instantsearch.snippet({
                         attribute: "body",
                         hit: data
                     })}</p>
