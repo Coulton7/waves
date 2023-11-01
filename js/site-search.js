3b5123a0c56b653d7b2780253d5d8b09be21c649
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-11-01 14:01:23 +0000

    Trying a method with article again

--- js/site-search.js
+++ js/site-search.js
@@ -189,8 +189,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         for (var x = 0; x < refineType.length; x++) {
 
-            if(refineType[x].includes('article')) {
-                refineType[x].replace("article", "Article").textContent;
+            if(refineType[x].innerText.includes('article')) {
+                refineType[x].textContent = refineType[x].textContent.replace("article", "Article");
             } else if (refineType[x].innerHTML.includes('page')) {
                 refineType[x].innerHTML = refineType[x].replace("page","Web Page");
             } else if (refineType[x].innerHTML.includes('apiplans')) {
