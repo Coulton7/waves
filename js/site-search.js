48b11541831c995c476562c9c6a83c2bdeedd9ae
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-11-01 13:01:40 +0000

    Reset

--- js/site-search.js
+++ js/site-search.js
@@ -189,8 +189,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         for (var x = 0; x < refineType.length; x++) {
 
-            if(refineType[x].getAttribute('value')==='article') {
-                refineType[x].getAttribute('value') = refineType[x].getAttribute('value').replace("article", "Article");
+            if(refineType[x].innerHTML.includes('article')) {
+                refineType[x].innerHTML = refineType[x].innerHTML.replace("article", "Article");
             } else if (refineType[x].innerHTML.includes('page')) {
                 refineType[x].innerHTML = refineType[x].replace("page","Web Page");
             } else if (refineType[x].innerHTML.includes('apiplans')) {
