8e05f7090321033bd1b453b8d9fd56f4c1a1f52e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2023-11-02 11:10:34 +0000

    Going another level down to see result

--- js/site-search.js
+++ js/site-search.js
@@ -158,7 +158,7 @@ document.addEventListener("DOMContentLoaded", function() {
     search.on('render', function() {
         var contentType = document.querySelectorAll('#contentCat');
         var vocabName = document.querySelectorAll("#vocabCat");
-        var refineType = document.querySelector("#types-list .ais-RefinementList-list");
+        var refineType = document.querySelectorAll("#types-list .ais-RefinementList-list .types-item");
         for (var i = 0; i < contentType.length; i++) {
 
             if(contentType[i].textContent == 'article') {
