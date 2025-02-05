c26f360aa6f4283901d7452c2c0e0cf0f9273173
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-05 13:21:49 +0000

    Trying an autocomplete.js

--- js/site-search.js
+++ js/site-search.js
@@ -5,11 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var filterLang = urlLang;
 
     const searchClient = algoliasearch('ZUQNGEX563', '23e29710cc4469dec35bd50bc2164b3a');
+    const indexName = 'aesseal'
+
+    const searchRouter = historyRouter();
     
     const search = instantsearch({
-        indexName: 'aesseal',
         searchClient,
-        routing: true,
+        indexName: indexName,
+        routing: searchRouter,
         searchFunction(helper) {
             if (helper.state.query === '')
             {
@@ -19,124 +22,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    let typeMapping;
-
-    if (filterLang == "en") {
-         typeMapping = {
-            '3dmodels': '3D Models',
-            'academy': 'Academy',
-            'apiplans': 'API Plans',
-            'article': 'Article',
-            'bearingprotection': 'Bearing Protection',
-            'cartridgemechanicalseals': 'Cartridge Mechanical Seals',
-            'casestudies': 'Case Studies',
-            'componentseals': 'Component Seals',
-            'elastomers': 'Elastomers',
-            'gasseals': 'Gas Seals',
-            'glandpacking': 'Gland Packing',
-            'locations': 'Locations',
-            'page': 'Web Page',
-            'productbrochure': 'Product Brochure',
-            'sealsupportsystems': 'Seal Support Systems',
-            'video': 'Video',
-            'whitepaper': 'Whitepaper',
-        }
-    } else if (filterLang == "waves") {
-         typeMapping = {
-            '3dmodels': 'Modèles 3D',
-            'academy': 'Académie',
-            'apiplans': 'API Plans',
-            'article': 'Article',
-            'bearingprotection': 'Protections de Paliers',
-            'cartridgemechanicalseals': 'Garnitures cartouches',
-            'casestudies': 'Études de cas',
-            'componentseals': 'Garnitures mécaniques bi-composants',
-            'elastomers': 'Elastomers',
-            'gasseals': 'Garnitures gaz',
-            'glandpacking': 'Tresses d’étanchéité',
-            'locations': 'Emplacement',
-            'page': 'Web Page',
-            'productbrochure': 'Brochures des produits',
-            'sealsupportsystems': 'Seal Support Systems',
-            'video': 'Vidéos',
-            'whitepaper': 'Whitepaper',
-        }
-    }
-
-    const vidMapping ={
-        'industry': 'Industry',
-        'glandpacking': 'Gland Packing Category',
-        'sealsupportsystems': 'Seal Support Systems Category',
-        'bearingprotectioncategories': 'Bearing Protection Category',
-        'cartridgemechanicalseals': 'Cartridge Mechanical Seals Category',
-        'gasseals': 'Gas Seals Category',
-        'componentseals': 'Component Seals Category',
-    }
-
-    const langlistPanel = instantsearch.widgets.panel ({
-        templates: {
-            header: '<h4>Select your Language</h4>'
-        },cssClasses: {
-            root: 'pt-5'
-        }
-    })(instantsearch.widgets.refinementList);
-
-    const typelistPanel = instantsearch.widgets.panel ({
-        templates: {
-            header: '<h4>Filter by Content Type</h4>'
-        },
-        cssClasses: {
-            root: 'pt-3'
-        }
-    })(instantsearch.widgets.refinementList);
-
-    search.addWidgets([{
-        init: function(options) {
-            if(filterLang == "waves")
-            {
-                options.helper.toggleRefinement('search_api_language', 'en');
-            }
-            else if(filterLang == "es")
-            {
-                options.helper.toggleRefinement('search_api_language', 'es');
-            }
-            else if (filterLang === "fr") {
-                options.helper.toggleRefinement('search_api_language', 'fr');
-            }
-            else if (filterLang === "de") {
-                options.helper.toggleRefinement('search_api_language', 'de');
-            }
-            else if(filterLang == "it")
-            {
-                options.helper.toggleRefinement('search_api_language', 'iy');
-            }
-            else if(filterLang == "pl")
-            {
-                options.helper.toggleRefinement('search_api_language', 'pl');
-            }
-            else if(filterLang == "ru")
-            {
-                options.helper.toggleRefinement('search_api_language', 'ru');
-            }
-            else if(filterLang == "tr")
-            {
-                options.helper.toggleRefinement('search_api_language', 'tr');
-            }
-            else if(filterLang == "zh-hans")
-            {
-                options.helper.toggleRefinement('search_api_language', 'zh-hans');
-            }
-        }
-      }]);
+    const virtualSearchBox = connectSearchBox(() => {});
     
     search.addWidgets([
-        instantsearch.widgets.configure({
+        virtualSearchBox({}),
+        pagination({
+            container: '#pagination',
+            totalPages: 3,
+            scrollTo: '#searchbox'
+        }),
+        configure({
             hitsPerPage: 20,
             attributesToSnippet: ['description:80', 'body:80'],
             page: 0,
         }),
-
-        instantsearch.widgets.clearRefinements({
+        clearRefinements({
             container: '#clear-refinements',
             cssClasses:{
                 root: 'pt-5',
@@ -145,12 +45,10 @@ document.addEventListener("DOMContentLoaded", function() {
                 ]
             }
         }),
-
         langlistPanel({
             container: '#lang-list',
             attribute: 'search_api_language',
             templates: {
-                header: 'Select your Language',
                 item: '<input type="checkbox" class="ais-refinement-list--checkbox lang-item" value="{{label}}" {{#isRefined}}checked="true"{{/isRefined}}> {{label}} <span class="ais-refinement-list--count">({{count}})</span>',
             },
             transformItems(items){
@@ -161,45 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             sortBy: ['isRefined', 'count:desc', 'name:asc']
         }),
-
-        typelistPanel({
-            container: '#type-list',
-            attribute: 'type',
-            templates: {
-                header: 'Filter by Content Type',
-                item: '<input type="checkbox" class="ais-refinement-list--checkbox" {{#isRefined}}checked="true"{{/isRefined}}> {{label}} <span class="ais-refinement-list--count">({{count}})</span>',
-            },
-            transformItems(items){
-                return items.map(item => ({
-                    ...item,
-                    label: typeMapping[item.label],
-                }));
-            },
-            cssClasses: {
-                item: ['types-item']
-            },
-            sortBy: ['isRefined', 'count:desc', 'name:asc']
-        }),
-    
-        instantsearch.widgets.pagination({
-            container: '#pagination',
-            totalPages: 3,
-            scrollTo: '#searchbox'
-        }),
-    
-        instantsearch.widgets.searchBox({
-            container: '#searchbox',
-            placeholder: 'Enter Your Keywords',
-            searchAsYouType: false,
-            cssClasses: {
-                form : 'search-block',
-                input: 'form-control',
-                submit: 'btn btn-primary',
-                submitIcon: 'text-white'
-            }
-        }),
-
-        instantsearch.widgets.stats({
+        stats({
             container: '#stats',
             templates: {
                 text(data, { html }) {
@@ -216,40 +76,81 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
             }
         }),
-    
-        instantsearch.widgets.hits ({
+        hits({
             container: '#hits',
             templates:{
-                item: data => `
-                <div class="search-result">
-                    <small>${data.url}</small>
-                    <p class="h3 ${data.title ? '' : 'display-none'}">${data.title}</p>
-                    <p class="h3 ${data.name_1 ? '' : 'display-none'}">${data.name_1}</p>
-                    <p id="contentCat" class="lead ${data.type ? '' : 'display-none'}">${data.type}</p>
-                    <p id="vocabCat" class="lead ${data.vid ? '' : 'display-none'}">${data.vid}</p>
-                    <p class=${data.description ? '' : 'display-none'}>${instantsearch.snippet({
-                        attribute: "description",
-                        hit: data
-                    })}</p>
-                    <p class=${data.body ? '' : 'display-none'}>${instantsearch.snippet({
-                        attribute: "body",
-                        hit: data
-                    })}</p>
-                    <a class="btn btn-primary align-self-end" href="${data.url}">Read More</a>
-                </div>`,
-                empty: `<p class="h3">No results found matching {{query}}</p>
-                <p>Sorry we couldn’t find a result for your search. Try to search again by, checking your search for spelling mistakes and/or reducing the number of keywords used. You can also try using a broader search phrase.</p>'
-                <p class="h3">Are you searching for a Part Number or Serial Number?</p>`,
-            },
-            transformItems(items){
-                return items.map(item => ({
-                    ...item,
-                    type: typeMapping[item.type],
-                    vid: vidMapping[item.vid]
-                }))
+                    item(data, { html, components }){
+                        if(filterLang == 'en'){
+                            return html `<div class="search-result" data-insights-object-id="${data.objectID}" data-insights-position="${data.__position}" data-insights-query-id="${data.__queryID}">
+                                <small>${data.url}</small>
+                                <p class="h3 ${data.title ? '' : 'd-none'}">${data.title}</p>
+                                <p class="h3 ${data.name_1 ? '' : 'd-none'}">${data.name_1}</p>
+                                <p id="contentCat" class="lead ${data.type ? '' : 'd-none'}">${data.type}</p>
+                                <p id="vocabCat" class="lead ${data.vid ? '' : 'd-none'}">${data.vid}</p>
+                                <p class=${data.description ? '' : 'd-none'}>${components.Snippet({
+                                    attribute: "description",
+                                    hit: data,
+                                    highlightedTagName: 'strong'
+                                })}</p>
+                                <p class=${data.body ? '' : 'd-none'}>${components.Snippet({
+                                    attribute: "body",
+                                    hit: data,
+                                    highlightedTagName: 'strong'
+                                })}</p>
+                                <a class="btn btn-primary view-details align-self-end" href="${data.url}">Read More</a>
+                            </div>`
+                        }
+                    }
             }
-        })
+        }),
     ]);
     
     search.start();
+
+    function setInstantSearchUiState(indexUiState) {
+        search.setUiState(uiState => ({
+            ...uiState,
+            [indexName]: {
+                ...uiState[indexName],
+                page: 1,
+                ...indexUiState
+            },
+        }));
+    }
+
+    function getInstantSearchUiState () {
+        const uiState = instantSearchRouter.read();
+
+        return(uiState && uiState[indexName]) || {};
+    }
+
+    const searchPageState = getInstantSearchUiState();
+
+    let skipInstantSearchUiStateUpdate = false;
+    const { setQuery } = autocomplete({
+        container: '#autocomplete',
+        placeholder: 'Enter your keywords',
+        detachedMediaQuery: 'none',
+        initialState: {
+            query: searchPageState.query || '',
+        },
+        onSubmit({ state }){
+            setInstantSearchUiState({ query: state.query });
+        },
+        onReset() {
+            setInstantSearchUiState({ query: '' });
+        },
+        onStateChange({ prevState, state }) {
+            if (!skipInstantSearchUiStateUpdate && prevState.query !== state.query) {
+                setInstantSearchUiState({ query:state.query });
+            }
+            skipInstantSearchUiStateUpdate = false;
+        }
+    })
+
+    window.addEventListener('popstate', () => {
+        skipInstantSearchUiStateUpdate = true;
+        setQuery(search.helper?.state.query || '');
+    });
+    
 });
